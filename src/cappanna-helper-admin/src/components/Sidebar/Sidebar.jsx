d82d4ec14c26b2bf680de0280c6fa348b693a2ab
Andrea Pierangeli <andrea.pierangeli@gmail.com>
2018-03-26 15:25:41 +0200

    Scaffold administration app [WIP]

---
+++ src/cappanna-helper-admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,127 @@
+import { HeaderLinks } from "components";
+import {
+  Drawer,
+  Hidden,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  withStyles
+} from "material-ui";
+import { NavLink } from "react-router-dom";
+import cx from "classnames";
+import PropTypes from "prop-types";
+import React from "react";
+import sidebarStyle from "variables/styles/sidebarStyle.jsx";
+
+const Sidebar = ({ ...props }) => {
+  function activeRoute(routeName) {
+    return props.location.pathname.indexOf(routeName) > -1 ? true : false;
+  }
+  
+  const { classes, color, logo, image, logoText, routes } = props;
+  const links = (
+    <List className={classes.list}>
+      {routes.map((prop, key) => {
+        
+        if (prop.redirect) {
+          return null;
+        }
+
+        const listItemClasses = cx({
+          [" " + classes[color]]: activeRoute(prop.path)
+        });
+        const whiteFontClasses = cx({
+          [" " + classes.whiteFont]: activeRoute(prop.path)
+        });
+
+        return (
+          <NavLink
+              to={prop.path}
+              className={classes.item}
+              activeClassName="active"
+              key={key}
+          >
+            <ListItem button className={classes.itemLink + listItemClasses}>
+              <ListItemIcon className={classes.itemIcon + whiteFontClasses}>
+                <prop.icon />
+              </ListItemIcon>
+              <ListItemText
+                  primary={prop.sidebarName}
+                  className={classes.itemText + whiteFontClasses}
+                  disableTypography
+              />
+            </ListItem>
+          </NavLink>
+        );
+      })}
+    </List>
+  );
+
+  const brand = (
+    <div className={classes.logo}>
+        <NavLink to="dashboard" className={classes.logoLink}>
+          <div className={classes.logoImage}>
+            <img src={logo} alt="logo" className={classes.img} />
+          </div>
+          {logoText}
+        </NavLink>
+    </div>
+  );
+
+  return (
+    <div>
+      <Hidden mdUp>
+        <Drawer
+            variant="temporary"
+            anchor="right"
+            open={props.open}
+            classes={{
+            paper: classes.drawerPaper
+          }}
+            onClose={props.handleDrawerToggle}
+            ModalProps={{
+            keepMounted: true
+          }}
+        >
+          {brand}
+          <div className={classes.sidebarWrapper}>
+            <HeaderLinks />
+            {links}
+          </div>
+          {image !== undefined ? (
+            <div
+                className={classes.background}
+                style={{ backgroundImage: "url(" + image + ")" }}
+            />
+          ) : null}
+        </Drawer>
+      </Hidden>
+      <Hidden smDown>
+        <Drawer
+            anchor="left"
+            variant="permanent"
+            open
+            classes={{
+            paper: classes.drawerPaper
+          }}
+        >
+          {brand}
+          <div className={classes.sidebarWrapper}>{links}</div>
+          {image !== undefined ? 
+            <div
+                className={classes.background}
+                style={{ backgroundImage: "url(" + image + ")" }}
+            />
+           : null}
+        </Drawer>
+      </Hidden>
+    </div>
+  );
+};
+
+Sidebar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(sidebarStyle)(Sidebar);
---
+++ src/cappanna-helper-admin/src/components/index.js
@@ -0,0 +1,57 @@
+import A from "./Typography/A.jsx";
+import Button from "./CustomButtons/Button.jsx";
+import ChartCard from "./Cards/ChartCard.jsx";
+import CustomInput from "./CustomInput/CustomInput.jsx";
+import Danger from "./Typography/Danger.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Header from "./Header/Header.jsx";
+import HeaderLinks from "./Header/HeaderLinks.jsx";
+import IconButton from "./CustomButtons/IconButton.jsx";
+import Info from "./Typography/Info.jsx";
+import ItemGrid from "./Grid/ItemGrid.jsx";
+import Muted from "./Typography/Muted.jsx";
+import P from "./Typography/P.jsx";
+import Primary from "./Typography/Primary.jsx";
+import ProfileCard from "./Cards/ProfileCard.jsx";
+import Quote from "./Typography/Quote.jsx";
+import RegularCard from "./Cards/RegularCard.jsx";
+import Sidebar from "./Sidebar/Sidebar.jsx";
+import Small from "./Typography/Small.jsx";
+import Snackbar from "./Snackbar/Snackbar.jsx";
+import SnackbarContent from "./Snackbar/SnackbarContent.jsx";
+import StatsCard from "./Cards/StatsCard.jsx";
+import Success from "./Typography/Success.jsx";
+import Table from "./Table/Table.jsx";
+import Tasks from "./Tasks/Tasks.jsx";
+import TasksCard from "./Cards/TasksCard.jsx";
+import Warning from "./Typography/Warning.jsx";
+
+export {
+  ChartCard,
+  ProfileCard,
+  RegularCard,
+  StatsCard,
+  TasksCard,
+  Button,
+  IconButton,
+  CustomInput,
+  Footer,
+  ItemGrid,
+  Header,
+  HeaderLinks,
+  Sidebar,
+  Snackbar,
+  SnackbarContent,
+  Table,
+  Tasks,
+  P,
+  Quote,
+  Muted,
+  Primary,
+  Info,
+  Success,
+  Warning,
+  Danger,
+  Small,
+  A
+};
