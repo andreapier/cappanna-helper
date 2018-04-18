401b95b24cc484785729aec75aef3cfabfcb443c
Andrea Pierangeli <andrea.pierangeli@gmail.com>
2018-04-18 15:01:48 +0200

    [App] Update dependencies [WIP]

--- src/cappanna-helper-app/src/index.js
+++ src/cappanna-helper-app/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/app/App";
-import registerServiceWorker from "./registerServiceWorker";
+import App from "components/app/App";
+//import registerServiceWorker from "./registerServiceWorker";
 import storeGenerator from "./store";
-import { loadUserData } from "./actions";
+import { loadUserData } from "actions";
 
 const store = storeGenerator();
 store.dispatch(loadUserData());
@@ -16,9 +16,9 @@ const renderApp = () => {
 renderApp();
 
 if (module.hot) {
-  module.hot.accept("./components/app/App", () => {
+  module.hot.accept("components/app/App", () => {
     renderApp();
   });
 }
 
-registerServiceWorker();
+//registerServiceWorker();
