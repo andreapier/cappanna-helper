709257f890d6c9d82fccfd7a3012ee53b430fa47
Andrea Pierangeli <andrea.pierangeli@gmail.com>
2018-05-28 12:24:15 +0200

    [App] Fix wait dialog style

--- src/cappanna-helper-app/src/components/WaitDialog/index.js
+++ src/cappanna-helper-app/src/components/WaitDialog/index.js
@@ -5,25 +5,23 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import Grid from "@material-ui/core/Grid";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import ItemGrid from "components/Grid/ItemGrid";
-
-const style = {};
+import waitDialogStyle from "variables/styles/waitDialogStyle";
 
 const WaitDialog = props => {
   const { classes, loading, message } = props;
 
   return (
-    <Dialog open={loading}>
-      <DialogTitle>{"Attendere"}</DialogTitle>
-      <DialogContent>
+    <Dialog
+      open={loading}
+      disableBackdropClick
+      disableEscapeKeyDown
+      className={classes.root}
+    >
+      <DialogTitle className={classes.title}>{"Attendere"}</DialogTitle>
+      <DialogContent className={classes.content}>
+        <CircularProgress size={60} thickness={7} />
         <DialogContentText>{message}</DialogContentText>
-        <Grid container direction="column">
-          <ItemGrid xs={12}>
-            <CircularProgress size={60} thickness={7} />
-          </ItemGrid>
-        </Grid>
       </DialogContent>
     </Dialog>
   );
@@ -35,4 +33,4 @@ WaitDialog.propTypes = {
   message: PropTypes.string.isRequired
 };
 
-export default withStyles(style)(WaitDialog);
+export default withStyles(waitDialogStyle)(WaitDialog);
---
+++ src/cappanna-helper-app/src/variables/styles/waitDialogStyle.js
@@ -0,0 +1,14 @@
+const waitDialogStyle = {
+  title: {
+    textAlign: "center"
+  },
+  paper: {
+    width: "300px",
+    height: "300px"
+  },
+  content: {
+    textAlign: "center"
+  }
+};
+
+export default waitDialogStyle;
