420b8b20d5f14ee2da4dd1e45e08988cc2199925
Andrea Pierangeli <a.pierangeli@apra.it>
2018-06-15 18:28:33 +0200

    [App] Fix order list infinite render [WIP]

--- src/cappanna-helper-app/src/containers/Orders/List/ConnectedOrdersList.js
+++ src/cappanna-helper-app/src/containers/Orders/List/ConnectedOrdersList.js
@@ -1,22 +1,13 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { loadOrderRequested, loadOrdersListRequested } from "actions";
 import List from "components/Orders/List";
 
 class ConnectedOrdersList extends Component {
-  // componentDidMount() {
-  //   this.props.loadOrdersListRequested();
-  // }
-
   render() {
     return <List {...this.props} />;
   }
 }
-
-ConnectedOrdersList.propTypes = {
-  loadOrderRequested: PropTypes.func.isRequired
-};
 
 const mapStateToProps = state => {
   return {
--- src/cappanna-helper-app/src/reducers/orders.js
+++ src/cappanna-helper-app/src/reducers/orders.js
@@ -1,11 +1,28 @@
 import { LOAD_ORDERS_LIST_COMPLETED, ORDER_CREATED } from "actions/types";
 
 const initialStatus = [];
+const areEqual = (prev, next) => {
+  if (prev.length !== next.length) {
+    return false;
+  }
+
+  for (let i = 0, l = prev.length; i < l; i++) {
+    console.log(prev[i].id, next[i].id);
+    if (prev[i].id !== next[i].id) {
+      return false;
+    }
+  }
+
+  return true;
+};
 
 export default function(state = initialStatus, action) {
   switch (action.type) {
     case LOAD_ORDERS_LIST_COMPLETED:
-      return [...action.payload];
+      if (!areEqual(state, action.payload)) {
+        return [...action.payload];
+      }
+      return state;
 
     case ORDER_CREATED:
       return [...state, action.payload];
