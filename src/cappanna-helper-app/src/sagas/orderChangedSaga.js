267a70a538632de17ceab49e311c1b244e3cbf5c
Andrea Pierangeli <a.pierangeli@centro3cad.it>
2018-08-07 15:08:04 +0200

    [App] Handle order edit

--- src/cappanna-helper-app/src/sagas/orderChangedSaga.js
+++ src/cappanna-helper-app/src/sagas/orderChangedSaga.js
@@ -5,8 +5,9 @@ import { notifyWarning, resetOrder } from "actions";
 
 function* orderChanged(action) {
   const state = yield select();
-
-  if (state.newOrder.id === action.payload.id) {
+  const lastOperation = action.payload.operations.slice(-1)[0];
+  
+  if (state.newOrder.id === action.payload.id && lastOperation.userId !== state.user.userId) {
     yield put(notifyWarning("L'ordine è cambiato, ripetere le modifiche"));
     yield put(resetOrder());
     history.push(`order/${action.payload.id}`);
