339c44337bb95b0740778a3537fa4299938cfc69
andreapier <andrea.pierangeli@gmail.com>
2022-09-25 11:50:30 +0200

    Update to net6.0

--- src/cappanna-helper-api/CappannaHelper.Api/Persistence/ApplicationDbContext.cs
+++ src/cappanna-helper-api/CappannaHelper.Api/Persistence/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using CappannaHelper.Api.Identity.DataModel;
 using CappannaHelper.Api.Identity.Extensions;
 using CappannaHelper.Api.Persistence.Mapping;
@@ -29,6 +30,19 @@ namespace CappannaHelper.Api.Persistence
             modelBuilder
                 .MapIdentityEntities()
                 .MapOrderEntities();
+
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+
+                    foreach (var property in properties)
+                    {
+                        modelBuilder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
+                    }
+                }
+            }
         }
     }
 }
